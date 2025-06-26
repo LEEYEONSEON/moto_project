040d6cb628b883c09f89a3c87af9b3bdf73d7ceb
DESKTOP-JDPNBAH\user1 <tkddbspsy1016@gmail.com>
2025-06-26 14:17:30 +0900

    첫 번 째 커밋

---
+++ moto_frontend/moto/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './component/common/Header';
+import Footer from './component/common/Footer';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='wrap'>
+      <Header/>
+
+      <Footer/>
+    </div>
+
+  )
+}
+
+export default App
