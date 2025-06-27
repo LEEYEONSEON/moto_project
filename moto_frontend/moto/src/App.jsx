ffff9f56ad73558f01df95b87b93469f7d7a5ef8
DESKTOP-O2PN775\user1 <lg23831885@gmail.com>
2025-06-27 16:29:03 +0900

    sidebar App.jsx에 설정하여 모든페이지에서 볼 수 있도록 변경, sidebar 개선 및 버튼 클릭시 DetailSideBar.jsx 생성 및 create

--- moto_frontend/moto/src/App.jsx
+++ moto_frontend/moto/src/App.jsx
@@ -2,13 +2,21 @@ import Header from './component/common/Header';
 import Footer from './component/common/Footer';
 
 import './App.css'
-import Main from './component/Main';
+import PageMain from './component/PageMain';
+import Sidebar from './component/common/Sidebar';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='wrap'>
       <Header/>
-      <Main />
+      <main className='content' style={{display:"flex"}}>
+      <Sidebar/>
+      <Routes>
+      <Route path='/' element={<PageMain />} />
+      
+      </Routes>
+      </main>
       <Footer/>
     </div>
 
