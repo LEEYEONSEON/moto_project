a41399963174818be23323a1210d6edc5f9ef192
DESKTOP-JT5A4KE\user1 <nyamnyamzupzup@gmail.com>
2025-06-27 17:14:16 +0900

    헤더 로고 누르면 메인페이지로 이동하도록 수정

--- moto_frontend/moto/src/component/common/Header.jsx
+++ moto_frontend/moto/src/component/common/Header.jsx
@@ -17,7 +17,7 @@ export default function Header () {
         <header className="header">
             <div>
                 <div className="logo">
-                    <Link to="#">MoTo</Link>
+                    <Link to="/">MoTo</Link>
                 </div>
                 <MainNavi />
                 <HeaderLink />
