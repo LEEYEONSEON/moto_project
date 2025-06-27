cf136711f5b4601c6cd333dc4081c7169085841c
DESKTOP-JT5A4KE\user1 <nyamnyamzupzup@gmail.com>
2025-06-27 16:45:17 +0900

    pull 받기 위해 작동 되는것들만 commit

--- moto_frontend/moto/src/App.jsx
+++ moto_frontend/moto/src/App.jsx
@@ -1,5 +1,8 @@
 import Header from './component/common/Header';
 import Footer from './component/common/Footer';
+import WalletPage from './component/common/WalletPage';
+import { Routes, Route } from 'react-router-dom';
+
 
 import './App.css'
 import Main from './component/Main';
@@ -8,6 +11,9 @@ function App() {
   return (
     <div className='wrap'>
       <Header/>
+      <Routes>
+        <Route path="/users/me/wallet" element={<WalletPage />} />
+      </Routes>
       <Main />
       <Footer/>
     </div>
--- moto_frontend/moto/src/component/Main.jsx
+++ moto_frontend/moto/src/component/Main.jsx
@@ -1,14 +1,17 @@
 import Sidebar from "./common/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import WalletPage from './common/WalletPage';
 
-export default function Main(){
-
-    return (
-        <>
-         <section className="section" style={{width:"80%", backgroundColor:"black", display:"flex"}}>
-        <Sidebar />
-            <div className="page-title" style={{color:"white"}}>메인페이지</div>
-            
-        </section>
-        </>
-    )
+export default function Main() {
+  return (
+    <section className="section" style={{ width: "100%", display: "flex", backgroundColor: "black" }}>
+      <Sidebar />
+      <div style={{ flex: 1, padding: "20px", color: "white" }}>
+        <Routes>
+          <Route path="/" element={<div>메인페이지</div>} />
+          <Route path="/users/me/wallet" element={<WalletPage />} />
+        </Routes>
+      </div>
+    </section>
+  );
 }
