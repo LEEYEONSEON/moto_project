836052f662f38548fc4f991e960411a999cd6d1f
DESKTOP-JT5A4KE\user1 <nyamnyamzupzup@gmail.com>
2025-06-27 17:17:33 +0900

    삭제한 파일임 백업파일이랑 기존에 멤버 파일이 존재해서 삭제함.

--- moto_backend/moto/src/main/java/kr/or/iei/member/model/dao/WalletDao.java
+++
@@ -1,8 +0,0 @@
-package kr.or.iei.member.model.dao;
-
-import kr.or.iei.member.model.dto.WalletSummary;
-
-public interface WalletDao {
-    // userNo로 지갑 요약 정보 조회
-    WalletSummary selectWalletSummary(String userNo);
-}
