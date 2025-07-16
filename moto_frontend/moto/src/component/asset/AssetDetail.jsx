a007dfacfaa0651784deeaa48f7d8bd24e77189d
정다은 <daeunjeongkhacademy@gmail.com>
2025-07-16 14:07:14 +0900

    asset Detail / 차트 컴포넌트 삭제 함 - 정다은

--- moto_frontend/moto/src/component/asset/AssetDetail.jsx
+++
@@ -1,9 +0,0 @@
-export default function AssetDetail() {
-  return (
-    <>
-    <section className="section asset-detail">
-      <h1>assetDetail</h1>
-    </section>
-    </>
-  );
-}
--- moto_frontend/moto/src/component/asset/AssetPage.jsx
+++ moto_frontend/moto/src/component/asset/AssetPage.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import AssetList from "./AssetList";
-import AssetDetail from "./AssetDetail";
 
 export default function AssetPage() {
 
     return(
         <>
         <Routes>
-            <Route path=":assetCode" element={<AssetDetail />} />  
             <Route path="" element={<AssetList />} />
         </Routes>
             
